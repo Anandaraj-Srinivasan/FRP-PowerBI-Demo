6394168693b169866409066b8f143c9c158cec5b
Anandaraj Srinivasan <anandaraj.srinivasan@maersk.com>
2022-08-02 12:43:19 +0530

    Add dynamic roles and User

--- Globomantics.PowerBI/RowLevelSecurity/IdentityProvider.cs
+++ Globomantics.PowerBI/RowLevelSecurity/IdentityProvider.cs
@@ -3,16 +3,84 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.PowerBI.Api.Models;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace Globomantics.PowerBI.RowLevelSecurity
 {
-    public class IdentityProvider: IIdentityProvider
+    public class IdentityProvider : IIdentityProvider
     {
+        public enum Roles
+        {
+            MaerskAdmin,
+            SurveyorCompanyAdmin,
+            SurveyorCompanyUser,
+            CommercialOwnerAdmin,
+            CommercialOwnerUser
+        }
+
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public IdentityProvider(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
+        }
+        public class UserRole
+        {
+            public string Name { get; set; }
+            public List<string> Roles { get; set; }
+        }
+
+
+        public List<string> GetRoles(string username)
+        {
+            var users = new List<UserRole>()
+            {
+                 new UserRole ()
+                 {
+                     Name = "Anand", Roles = new List<string>
+                                            {
+                                                Roles.MaerskAdmin.ToString(),
+                                                Roles.CommercialOwnerAdmin.ToString(),
+                                            }
+                 },
+                 new UserRole ()
+                 {
+                     Name = "nscadmin", Roles = new List<string>
+                                            {
+                                                Roles.CommercialOwnerAdmin.ToString(),
+                                            }
+                 },
+                 new UserRole ()
+                 {
+                     Name = "NSC", Roles = new List<string>
+                                            {
+                                                Roles.CommercialOwnerUser.ToString(),
+                                            }
+                 },
+                 new UserRole ()
+                 {
+                     Name = "nsbadmin", Roles = new List<string>
+                                            {
+                                                Roles.SurveyorCompanyAdmin.ToString(),
+                                            }
+                 },
+                 new UserRole ()
+                 {
+                     Name = "NSB", Roles = new List<string>
+                                            {
+                                                Roles.SurveyorCompanyUser.ToString(),
+                                            }
+                 }
+
+
+            };
+
+            var roles = users.Find(u => string.Equals(u.Name,username,System.StringComparison.OrdinalIgnoreCase));
+
+            if (roles == null)            
+                return null;
+            
+            return roles.Roles;
+
         }
 
         public EffectiveIdentity GetUserIdentity()
@@ -21,8 +89,8 @@ namespace Globomantics.PowerBI.RowLevelSecurity
             return new EffectiveIdentity
             {
                 Username = user,
-                Roles = new List<string> { "MaerskAdmin" }
-                //Roles = new List<string> { "GlobomanticsUser" }
+                Roles = GetRoles(user)
+                //Roles = new List<string> { "MaerskAdmin" }        
             };
         }
 
@@ -32,8 +100,8 @@ namespace Globomantics.PowerBI.RowLevelSecurity
             return new EffectiveIdentity
             {
                 Username = user,
-                Roles = new List<string> { "MaerskAdmin" },
-                //Roles = new List<string> { "GlobomanticsUser" },
+                Roles = GetRoles(user),
+                //Roles = new List<string> { "MaerskAdmin" },        
                 Datasets = new List<string> { datasetId }
             };
         }
